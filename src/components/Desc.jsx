48a2f7e5995eeccf26652f7814f071d232f902f5
athlrajm <athulrajm333@gmail.com>
2024-01-09 15:01:16 +0530

    fifth commit

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -5,7 +5,7 @@ const Desc = () => {
     <div className='Desc'>
         <span className='Desc1'>Create, play, and battle with friends for free in Fortnite. Be the last player standing in Battle Royale and Zero Build, experience a concert or live event, or discover over a million creator made games, including racing, parkour, zombie survival, and more. Each Fortnite island has an individual age rating so you can find the one that's right for you and your friends. Find it all in Fortnite ... Drop In.</span>
         
-        <button>Visit Website</button><br></br>
+        <button className='btn1'>Visit Website</button><br></br>
     </div>
   )
 }
