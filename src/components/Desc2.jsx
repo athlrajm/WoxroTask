cebcfa6856f478c8556f17af96ee18cb1cec50e0
athlrajm <athulrajm333@gmail.com>
2024-01-09 15:54:03 +0530

    seventh commit

--- src/components/Desc2.jsx
+++ src/components/Desc2.jsx
@@ -4,6 +4,7 @@ const Desc2 = () => {
   return (
     <div className='container'>
         <h3 className='Desc'>Our Contribution</h3>
+        
         <span className='Desc1'>Our core offering extends beyond mere profit generation; we emphasize the growth and active involvement of our user community. Collaborating with us represents an investment, rather than a mere expenditure. Our dedication to providing distinctive digital interactions guarantees unparalleled benefits for our clientele.</span>
     </div>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className='Footer'>
 
         <h3 className='Desc'>Interested in delving deeper into the project?</h3>
-        <span className='Desc1'>If you'd like to explore further details about our initiatives or any of our affiliated brands, don't hesitate to connect. You can reach out to us via email at hello@abc.com or give us a call at +91 480 20802730.</span>
+        <span className='Desc1'>If you'd like to explore further details about our initiatives or any of our </span><span className='Desc1'>affiliated brands, don't hesitate to connect. You can reach out to us via</span><span className='Desc1'> email at <b> hello@abc.com </b> or give us a call at <b>+91 480 20802730.</b></span>
         <div className='row'>
         <button className='bt1'>Ring Us On Skype</button> <button className='bt2'>Contact Us</button>
         </div>
