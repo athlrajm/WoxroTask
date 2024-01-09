51d55773aac71ed97d61aa645c46d54bc6c07302
athlrajm <athulrajm333@gmail.com>
2024-01-09 16:26:55 +0530

    final x commit

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -4,8 +4,8 @@ const Desc = () => {
   return (
     <div className='Desc'>
         <span className='Desc1'>Create, play, and battle with friends for free in Fortnite. Be the last player standing in Battle Royale and Zero Build, experience a concert or live event, or discover over a million creator made games, including racing, parkour, zombie survival, and more. Each Fortnite island has an individual age rating so you can find the one that's right for you and your friends. Find it all in Fortnite ... Drop In.</span>
-        
-        <button className='btn1'>Visit Website</button><br></br>
+        <a href='https://www.fortnite.com/?lang=en-US' className='social-link'>
+        <button className='btn1'>Visit Website</button></a><br></br>
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <h3 className='Desc'>Interested in delving deeper into the project?</h3>
         <span className='Desc1'>If you'd like to explore further details about our initiatives or any of our </span><span className='Desc1'>affiliated brands, don't hesitate to connect. You can reach out to us via</span><span className='Desc1'> email at <b> hello@abc.com </b> or give us a call at <b>+91 480 20802730.</b></span>
         <div className='row'>
-        <button className='bt1'>Ring Us On Skype</button>&nbsp;&nbsp;&nbsp;&nbsp; <button className='bt2'>Contact Us</button>
+        <a href='https://www.skype.com/en/' className='social-link'> <button className='bt1'>Ring Us On Skype</button></a>&nbsp;&nbsp;&nbsp;&nbsp; <a href='https://support.google.com/mail/answer/56256?hl=en' className='social-link'><button className='bt2'>Contact Us</button></a>
         </div>
 
         <div className='Desc'>
