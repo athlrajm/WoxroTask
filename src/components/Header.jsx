e58cf44e5ce5d0364134ff5a380675e65582325e
athlrajm <athulrajm333@gmail.com>
2024-01-09 16:56:49 +0530

    final1 commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { CgClose } from "react-icons/cg";
 import './Header.css'
 const Header = () => {
   return (
     <div className='header1'>
+        <CgClose className='close' /><br></br>
         <span className='Span1'>Epic Games : An American Video Game And Software Developer</span><br></br>
         <span className='Span1'> And Publisher Based In Cary,North Carolina</span>
     </div>
